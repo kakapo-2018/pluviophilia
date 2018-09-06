6ec9cc9017357766b668bde710e3d451cdcb4e61
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-09-06 18:09:55 +1200

    react hell

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+//import Leaf from './Leaf'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      leaves: [
+        {
+          x: 10,
+          y: 10
+        },
+        {
+          x: 50,
+          y: 50
+        }
+      ]
+    }
+    this.updateCanvas = this.updateCanvas.bind(this)
+  }
+
+  componentDidMount(){
+    this.updateCanvas()
+  }
+
+  updateCanvas(){
+    const ctx = this.refs.canvas.getContext('2d');
+    this.setState = {
+      ctx: ctx
+    }
+  }
+  render() {
+    console.log(this.state.ctx)
+    return (
+      <React.Fragment>
+        
+      <canvas ref="canvas" width="1920" height="1080"></canvas>
+      {this.state.leaves.map(leaf => {
+        return <h1>fuuuuuuuuu</h1>
+      })}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
 
+{/* <Leaf x={leaf.x} y={leaf.y} ctx={this.state.ctx}/> */}
