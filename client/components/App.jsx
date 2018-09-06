3ce206713ee8bd8b64370562753d591c4920ed2c
isabella-grigg-eyley <isabella.grigg.eyley@gmail.com>
2018-09-07 10:42:37 +1200

    bella

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,45 +11,24 @@ class App extends React.Component {
           y: 10
         },
         {
-          x: 500,
-          y: 500
+          x: 50,
+          y: 50
         }
       ]
     }
-    this.updateCanvas = this.updateCanvas.bind(this)
   }
 
-  componentDidMount(){
-    this.updateCanvas()
-  }
-
-  updateCanvas(){
-    const ctx = this.refs.canvas.getContext('2d');
-    this.setState({
-      ctx: ctx
-    })
-  }
   render() {
-    console.log(this.state.ctx)
-    if (this.state.ctx) {
-      console.log("truthy")
+      let k = -1
       return (
         <React.Fragment>
-          <canvas ref="canvas" width="1920" height="1080"></canvas>
           {this.state.leaves.map(leaf => {
-            return <Leaf x={leaf.x} y={leaf.y} ctx={this.state.ctx}/>
+            k++
+            return <Leaf key={k} x={leaf.x} y={leaf.y} ctx={this.state.ctx}/>
           })}
         </React.Fragment>
       )
-    }
-    else {
-      console.log("falsy")
-      return (<canvas ref="canvas" width="1920" height="1080"></canvas>)
-      //return null
-    }
   }
 }
 
 export default App
-
-{/* <Leaf x={leaf.x} y={leaf.y} ctx={this.state.ctx}/> */}
--- client/components/Leaf.jsx
+++ client/components/Leaf.jsx
@@ -5,30 +5,16 @@ export default class Leaf extends React.Component {
     super(props)
     this.state = {
       x: this.props.x,
-      y: this.props.y,
-      ctx: this.props.ctx
+      y: this.props.y
     }
   }
 
   render(){
-    try {
-      if (this.state.ctx) {
-        console.log(this.state.ctx)
-        return (
-          <React.Fragment>
-          {this.state.ctx.fillRect(this.state.x, this.state.y, 100, 100)}
-          <h3>fucking rectangles</h3>
-          </React.Fragment>
-        )
-      } else {
-        console.log("ctx not defined")
-        return null
-      }
-    } catch (error) {
-      console.log("I fucking hate react")
-      return null
-    }
-    
+    return (
+      <React.Fragment>
+        <img src="/leaf.svg" alt="leaf"/>
+        {console.log("leafrender")}
+      </React.Fragment>
+    )
   }
-
 }
